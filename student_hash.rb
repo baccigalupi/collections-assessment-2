bce32187072e703791a00588d8e03f7b3bdaa8f5
brady <bradydynamite@hotmail.com>
2014-10-31 10:18:53 -0700

    Update student_hash.rb

--- student_hash.rb
+++ student_hash.rb
@@ -1,51 +1,52 @@
 student_info = [
-"John" => {
+{
     firstname: "John",
     lastname: "Foley",
     email: "john@gschool.it",
     class: "Beginning Snark",
-  }
-"Kelsey" => {
+},
+{
     firstname: "Kelsey",
     lastname: "Sylwester",
     email: "sellie@gmail.com",
-    class: "Ruby Immersive"
-  }
-"Timothy" =>  {
+    class: "Ruby Immersive",
+  },
+
+{
     firstname: "Timothy",
     lastname: "Rama",
     email:"Tim.Rama@gmail.com",
     class: "Ruby Immersive",
-  }
-"Kane" =>  {
+  },
+{
     firstname:"Kane",
     lastname:"Baccigalupi",
     email:"Kane@gschool.it",
-    class:"C for dummies"
-  }
-"Nikita" =>  {
+    class:"C for dummies",
+  },
+{
     firstname: "Nikita",
     lastname:"Theodosius",
     email:"Nikita.Theo@gmail.com",
-    class: "Ruby Immersive"
-  }
-"Roddy" =>  {
+    class: "Ruby Immersive",
+  },
+{
     firstname: "Roddy",
     lastname: "Eldred",
     email: "roddy.el@gmail.com",
-    class: "Ruby Immersive"
-  }
-"Martha" =>  {
+    class: "Ruby Immersive",
+  },
+{
     firstname: "Martha",
     lastname: "Berner",
     email: "Martha@gschool.it",
-    class: "Time Travel for Dummies"
-  }
-"Kofi" =>  {
+    class: "Time Travel for Dummies",
+  },
+{
     firstname: "Kofi",
     lastname:"Thomas",
     email: "k.thomas@gmail.com",
     class: "Ruby Immersive",
-  }
+  },
 
 ]
